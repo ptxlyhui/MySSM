2b9f08a9323642a871d5ffd95f798e3e1ffcf490
ptxlyhui <1185703656@qq.com>
2019-08-16 00:51:13 +0800

    ssm-environment，增加了逆向工程，以及用这个方法生成的model、dao、mapper文件

--- src/main/java/com/lyhui/mapper/UserMapper.java
+++ src/main/java/com/lyhui/mapper/UserMapper.java
@@ -1,10 +1,15 @@
 package com.lyhui.mapper;
 
 import com.lyhui.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
 
     int delete(String name);
 
     User query(User user);
+
+    User add(@Param("user") User user, @Param("str") String str);
+
+    User queryById(User user);
 }
---
+++ src/test/java/generator.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* mybatis逆向工程执行方法
+* */
+public class generator {
+    @Test
+    public void auto() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //执行xml的路径
+        File configFile = new File("G:\\reverse_project\\generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+}
